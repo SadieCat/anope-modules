12633507df80b5eb120acee01d102f5dbd9b5981
Peter Powell <petpow@saberuk.com>
2016-01-18 23:28:01 +0000

    Add a module for remotely opering users on InspIRCd.
    This requires m_svsoper from inspircd-extras to be loaded on the
    remote server.

---
+++ os_opertype.cpp
@@ -0,0 +1,54 @@
+// OperServ OperType (C) 2016 Peter "SaberUK" Powell
+
+// Configuration:
+//   module { name = "os_opertype" }
+//   command { service = "OperServ"; name = "OPERTYPE"; command = "operserv/opertype"; permission = "operserv/opertype"; }
+
+#include "module.h"
+
+class OSOperTypeCommand : public Command
+{
+public:
+	OSOperTypeCommand(Module* creator)
+		: Command(creator, "operserv/opertype", 2, 2)
+	{
+		this->SetDesc("Sets the oper type of the specified user");
+		this->SetSyntax("\037nick\037 \037oper type\037");
+	}
+
+	void Execute(CommandSource& source, const std::vector<Anope::string>& parameters) anope_override
+	{
+		// Ensure that the target user is online.
+		User* target;
+		if (!(target = User::Find(parameters[0], true)))
+		{
+			source.Reply(NICK_X_NOT_IN_USE, parameters[0].c_str());
+			return;
+		}
+
+		// Broadcast the SVSOPER to the remote server.
+		UplinkSocket::Message() << "SVSOPER " << target->GetUID() << " :" << parameters[1].replace_all_cs(" ", "_");
+		source.Reply("\002%s's\002 oper type has been set to \002%s\002.", target->nick.c_str(), parameters[1].c_str());
+	}
+
+	bool OnHelp(CommandSource& source, const Anope::string&) anope_override
+	{
+		this->SendSyntax(source);
+		source.Reply(" ");
+		source.Reply("Sets the oper type of the specified user");
+		return true;
+	}
+};
+
+class OSOperType : public Module
+{
+private:
+	OSOperTypeCommand command;
+
+public:
+	OSOperType(const Anope::string& moduleName, const Anope::string& creator)
+		: Module(moduleName, creator, THIRD)
+		, command(this) { }
+};
+
+MODULE_INIT(OSOperType)
